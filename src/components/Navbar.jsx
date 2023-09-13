2cb54b6ec4c07dad867b09008ad5fb53570a5fb0
Javier Villamar <javiervillamarcaballero@gmail.com>
2023-09-13 10:29:37 -0400

    fixNavbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,28 @@
-import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
+import React from 'react'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 function NavbarMenu () {
   const generation = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
   return (
-    <Navbar expand='lg' className='bg-body-tertiary'>
+    <Navbar bg='dark' variant='dark' expand='lg'>
       <Container>
-        <Navbar.Brand href='/'>Poke-API</Navbar.Brand>
+        <Navbar.Brand as={Link} to='/'>Poke-API</Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='me-auto'>
-            <NavLink className='btn btn-light btn-lg' to='/pokeApi'>PokeDex</NavLink>
+            <Nav.Link as={Link} to='/pokeApi'>PokeDex</Nav.Link>
             <NavDropdown title='Generaciones'>
-              {
-                generation.map(gen => (
-                  <NavDropdown.Item key={gen} href={`/generation/${gen}`}>Generacion {gen}</NavDropdown.Item>
-                ))
-              }
+              {generation.map(gen => (
+                <NavDropdown.Item
+                  key={gen}
+                  as={Link}
+                  to={`/generation/${gen}`}
+                >
+                  Generación {gen}
+                </NavDropdown.Item>
+              ))}
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
@@ -24,65 +30,5 @@ function NavbarMenu () {
     </Navbar>
   )
 }
-
-// function asd () {
-//   return (
-//     <nav style={style.nav}>
-//       <ul style={style.ul}>
-//         <li style={style.li}>
-//           <NavLink style={style.a} to='/'>
-//             Home
-//           </NavLink>
-//         </li>
-//         <li style={style.li}>
-//           <NavLink style={style.a} to='/PokeApi'>
-//             PokeDex
-//           </NavLink>
-//         </li>
-//         <li style={style.li}>
-//           <NavLink style={style.a} to='/generations'>
-//             Generaciones
-//           </NavLink>
-//         </li>
-//       </ul>
-//     </nav>
-//   )
-// }
-
-// const style = {
-//   nav: {
-//     backgroundColor: '#333',
-//     padding: '10px 0',
-//     borderRadius: '20px 0px 0px 0px'
-//   },
-//   ul: {
-//     display: 'flex',
-//     justifyContent: 'center',
-//     flexWrap: 'wrap',
-//     alignItems: 'center',
-//     listStyle: 'none',
-//     padding: '0',
-//     margin: '0'
-//   },
-//   li: {
-//     padding: '10px 20px',
-//     margin: '0 5px',
-//     borderRadius: '5px',
-//     transition: 'background-color 0.3s ease',
-//     '&:hover': {
-//       backgroundColor: '#555'
-//     }
-//   },
-//   a: {
-//     color: '#fff',
-//     textDecoration: 'none',
-//     fontSize: '14px',
-//     padding: '5px 10px',
-//     borderRadius: '5px'
-//   },
-//   active: {
-//     backgroundColor: '#555'
-//   }
-// }
 
 export default NavbarMenu
